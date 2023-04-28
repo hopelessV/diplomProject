e59a0d8e5387da5999de667df72926a5ad204dec
Hopeless <Hopeless@DESKTOP-E3QKFVD>
2023-04-28 14:41:20 +0500

    .

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Hopeless\Source\Repos\hopelessV\diplomProject\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
